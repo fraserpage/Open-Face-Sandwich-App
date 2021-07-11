6797535cc9d2d2e07e4c42519e49fc3b0e2c6631
Mac <mac@Macs-MacBook-Pro.local>
2021-07-11 14:50:38 -0400

    Create the model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
-# Create your models here.
+
+
+class Slice(models.Model):
+    top = models.CharField(max_length=255)
+    middle = models.CharField(max_length=255)
+    bottom = models.CharField(max_length=255)
+    upload_date = models.DateField()
+    is_public = models.BooleanField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Sandwich(models.Model):
+    top = models.ForeignKey(Slice, on_delete=models.CASCADE)
+    middle = models.ForeignKey(Slice, on_delete=models.CASCADE)
+    bottom = models.ForeignKey(Slice, on_delete=models.CASCADE)
+    creation_date = models.CharField(max_length=50)
+    recent_update = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # included this in case you wanted to view the sandwich
+    # in a dedicated detail page
+    def get_absolute_url(self):
+        return reverse('sandwich_detail', kwargs={'pk': self.id})
