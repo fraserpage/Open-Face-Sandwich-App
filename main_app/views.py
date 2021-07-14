4d03fa2942c2ca866b8ca5c70864b7e7de97d6db
Mac <mac@Macs-MacBook-Pro.local>
2021-07-14 16:54:45 -0400

    Updated sign up controller

--- main_app/views.py
+++ main_app/views.py
@@ -347,7 +347,10 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('index')
+            if "next" in request.POST:
+                return redirect(request.POST['next'])
+            else:
+                return redirect('index')
         else:
             error_message = 'Invalid sign up - try again'
 
